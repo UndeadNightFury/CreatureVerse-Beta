219078fd91356011c8f8cd917d602558bb7740b9
UndeadNightFury <52829173+UndeadNightFury@users.noreply.github.com>
2021-03-11 13:18:04 -0500

    beta

--- js/play_game_test.js
+++
@@ -1,8 +0,0 @@
-function playgame() {
-  var r = confirm("Warning: Before progressing further, please read the following: This is a Narrative driven story that has been made to be realistic, as such there is a heavy presence of violence, gore, torture and other NSFW themes. It is strongly recommended that anybody not accustomed to these should not progress further as some scenes may cause distress!");
-
-  if (r == true) {
-	window.location.href = "data/species.html";
-  } else {
-  }
-}
