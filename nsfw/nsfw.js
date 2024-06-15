2052593c1a5ece54de8d6c85a133dcc7a977d83c
Skytalon Nightwing <undeadnightfury@gmail.com>
2024-06-15 10:17:19 -0400

    nsfw toggle prototype update

--- nsfw/nsfw.js
+++ nsfw/nsfw.js
@@ -1,18 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
+function updateNsfw()
+{
     var checkbox = document.querySelector('input[type="checkbox"]');
-  
-    checkbox.addEventListener('change', function () {
-      if (checkbox.checked) {
-        // do this
-        console.log('NSFW');
-        document.getElementById('sfw').style.visibility = 'hidden';
-        document.getElementById('nsfw').style.visibility = 'visible';
-      } 
-      else {
-        // do that
-        console.log('SFW');
-        document.getElementById('sfw').style.visibility = 'visible';
-        document.getElementById('nsfw').style.visibility = 'hidden';
-      }
-    });
-  });
+    var nsfw = localStorage.getItem("NsfwEnabled");
+    if (nsfw) {
+       // do this
+       console.log('NSFW');
+       document.getElementById('sfw').style.visibility = 'hidden';
+       document.getElementById('nsfw').style.visibility = 'visible';
+     } 
+     else {
+       // do that
+       console.log('SFW');
+       document.getElementById('sfw').style.visibility = 'visible';
+       document.getElementById('nsfw').style.visibility = 'hidden';
+     }
+}
+
+function initNsfwCheckbox()
+{
+    var checkbox = document.querySelector('input[type="checkbox"]');
+    var nsfw = localStorage.getItem("NsfwEnabled");
+    checkbox.checked = nsfw;
+    updateNsfw();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+   var checkbox = document.querySelector('input[type="checkbox"]');
+   checkbox.addEventListener('change', function () {
+       localStorage.setItem("NsfwEnabled", checkbox.checked);
+       updateNsfw();
+   });
+    initNsfwCheckbox();
+ });
